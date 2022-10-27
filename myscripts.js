514672152e33ef122f735aff05ecbe362e4380b0
Alex-Camacho <cuentascriptico@gmail.com>
2022-10-27 14:11:46 +0200

    Random color enabled, need ui improvement

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,4 @@
+//DEFAULT SIZE 16x16
 let container = document.getElementById("container");
 
 var columns = 16;
@@ -21,6 +22,23 @@ for(i = 1; i <= columns; i++){
 }
 }
 
+//16x16 button
+
+let button16 = document.getElementById("16px");
+button16.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square")
+    
+    squares.forEach(square => {  //Quita .square previos
+        square.remove();
+    });
+
+    columns = 16;
+    rows = 16;
+    setSquares();
+    document.getElementById("container").style.gridTemplateColumns = "repeat(16, auto)" ;
+})
+
+//64x64 button
 let button64 = document.getElementById("64px");
 button64.addEventListener("click", () => {
     const squares = document.querySelectorAll(".square")
@@ -35,16 +53,34 @@ button64.addEventListener("click", () => {
     document.getElementById("container").style.gridTemplateColumns = "repeat(64, auto)" ;
 })
 
-container.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "black";
+
+//Clear button
+let clearbutton = document.getElementById("clear");
+clearbutton.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square")
+
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    })
 })
 
-container.addEventListener("mouseout", (event) => {
+
+
+//Rainbow button
+let rainbow = document.getElementById("rainbow");
+rainbow.addEventListener("click", () => {
+    
+    container.addEventListener("mouseover", (event) => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        event.target.style.backgroundColor = "#" + randomColor;
+    })
+})
+
+
+//Pencil button
+let pencil = document.getElementById("pencil");
+pencil.addEventListener("click", () => {
+container.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "grey";
 })
-
-
-
-
-
-
+});
